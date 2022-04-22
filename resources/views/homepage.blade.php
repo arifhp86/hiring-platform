883f848f38812ad00760832a4e43d4d9015b0376
Arifur Rahman <arifhp86@gmail.com>
2022-04-22 11:32:34 +0600

    fix: homepage center alignment

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,12 +3,14 @@
 @section('title', 'Home')
 
 @section('content')
-    <div class="container">
-        <h1 class="p-40 text-4xl font-bold text-center">Welcome to the hiring platform</h1>
-        <div class="flex items-center justify-center">
-            <a href="/candidates-list" class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow align-middle">
-                Go to candidates list
-            </a>
+    <div class="container min-h-screen mx-auto">
+        <div class="flex flex-col justify-center items-center min-h-full">
+            <h1 class="text-2xl sm:text-4xl font-bold">Welcome to the hiring platform</h1>
+            <div class="flex items-center mt-10">
+                <a href="/candidates-list" class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow align-middle">
+                    Go to candidates list
+                </a>
+            </div>
         </div>
     </div>
 @endsection
