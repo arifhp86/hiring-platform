b6897c2f26111d909e1714fd180b1420f0a9a63b
Arifur Rahman <arifhp86@gmail.com>
2022-04-22 11:20:29 +0600

    refactor: introduce laravel layout file

--- resources/views/candidates/index.blade.php
+++ resources/views/candidates/index.blade.php
@@ -1,22 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>MZT test assignment</title>
+@section('title', 'Candidates')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;600;700&display=swap" rel="stylesheet">
-
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    </head>
-    <body>
-        <div id="app">
-            <div class="w-full p-6 bg-teal-100 text-right font-bold">Your wallet has: {{$coins ?? '?' }} coins</div>
-            <candidates :candidates="{{ json_encode($candidates) }}" />
-        </div>
-
-        <script src="{{ mix('/js/app.js') }}"></script>
-    </body>
-</html>
+@section('content')
+    <div class="w-full p-6 bg-teal-100 text-right font-bold">Your wallet has: {{$coins ?? '?' }} coins</div>
+    <candidates :candidates="{{ json_encode($candidates) }}" />
+@endsection
