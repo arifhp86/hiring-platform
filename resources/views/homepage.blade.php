1f00e64f2edaa157e50d42bdfe9e1701faecc85c
Arifur Rahman <arifhp86@gmail.com>
2022-04-23 22:42:44 +0600

    fix: transition

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,11 +3,11 @@
 @section('content')
     <transition
         enter-active-class="transition ease-out duration-200 transform"
-        enter-class="opacity-0 scale-95"
-        enter-to-class="opacity-100 scale-100"
-        leave-active-class="transition ease-in duration-75 transform"
-        leave-class="opacity-100 scale-100"
-        leave-to-class="opacity-0 scale-95"
+        enter-class="opacity-0"
+        enter-to-class="opacity-100"
+        leave-active-class="transition ease-out duration-200 transform"
+        leave-class="opacity-100"
+        leave-to-class="opacity-0"
     >
         <router-view></router-view>
     </transition>
