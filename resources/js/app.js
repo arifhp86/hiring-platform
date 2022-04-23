b0eaff9da75fc7b99375bc1527719599cc9adae8
Arifur Rahman <arifhp86@gmail.com>
2022-04-23 21:26:32 +0600

    feat: contact and hire front-end

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,10 @@ require('./bootstrap');
 import VueRouter from 'vue-router';
 import routes from './routes';
 import VueMeta from 'vue-meta';
+import VueSweetalert2 from 'vue-sweetalert2';
+
+
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 window.Vue = require('vue').default;
 
@@ -11,6 +15,8 @@ Vue.use(VueRouter);
 Vue.use(VueMeta, {
   refreshOnceOnNavigation: true
 });
+
+Vue.use(VueSweetalert2);
 
 const router = new VueRouter({
     mode: 'history',
--- resources/js/pages/Candidates.vue
+++ resources/js/pages/Candidates.vue
@@ -16,21 +16,23 @@
                 <div class="p-6 flex justify-between items-center">
                     <div>
                         <span v-if="hires.includes(candidate.id)" class="rounded-full px-3 py-1 text-sm font-semibold text-teal-500 flex">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
-                            </svg>
+                            <CheckIcon />
                             <span>Hired</span>
                         </span>
                         <span v-else-if="contacts.includes(candidate.id)" class="rounded-full text-sm font-semibold text-teal-500 flex">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
-                            </svg>
+                            <CheckIcon />
                             <span>Contacted</span>
                         </span>
                     </div>
-                    <div>
-                        <button v-if="! contacts.includes(candidate.id)" class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Contact</button>
-                        <button v-if="! hires.includes(candidate.id)" class="bg-white text-gray-800 font-semibold py-2 px-4 border border-gray-400 hover:bg-teal-100 rounded shadow ml-2">Hire</button>
+                    <div class="flex">
+                        <button v-if="! contacts.includes(candidate.id)" @click="confirmContact(candidate)" class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow flex items-center justify-between space-x-1">
+                            <LoadingIndicator v-if="processingContactRequest === candidate" />
+                            <span>Contact</span>
+                        </button>
+                        <button v-if="! hires.includes(candidate.id)" @click="confirmHire(candidate)" class="bg-white text-gray-800 font-semibold py-2 px-4 border border-gray-400 hover:bg-teal-100 rounded shadow ml-2 flex items-center justify-between space-x-1">
+                            <LoadingIndicator v-if="processingHireRequest === candidate" />
+                            <span>Hire</span>
+                        </button>
                     </div>
                 </div>
             </div>
@@ -40,11 +42,15 @@
 
 <script>
 import axios from 'axios';
-import AppLayout from "../layouts/AppLayout.vue";
+import AppLayout from "@/layouts/AppLayout.vue";
+import LoadingIndicator from '@/components/LoadingIndicator.vue';
+import CheckIcon from '@/components/CheckIcon.vue';
 
 export default {
     components: {
         AppLayout,
+        LoadingIndicator,
+        CheckIcon,
     },
     metaInfo: {
       title: 'Candidates',
@@ -54,15 +60,89 @@ export default {
             candidates: [],
             coins: 0,
             company: null,
+            newContacts: [],
+            newHires: [],
+            processingContactRequest: null,
+            processingHireRequest: null,
         };
     },
     computed: {
         contacts() {
-            console.log(this.company.contacts.map(item => item.id));
-            return this.company.contacts.map(item => item.id);
+            return [
+                ...this.company.contacts.map(item => item.id),
+                ...this.newContacts
+            ];
         },
         hires() {
-            return this.company.hires.map(item => item.id);
+            return [
+                ...this.company.hires.map(item => item.id),
+                ...this.newHires
+            ];
+        }
+    },
+    methods: {
+        confirmContact(candidate) {
+            this.$swal({
+                icon: 'info',
+                html: `Contacting <strong>${candidate.name}</strong> will cost <strong>5 coins</strong> from your wallet, click confirm button to proceed`,
+                showCancelButton: true,
+                confirmButtonText: 'Confirm'
+            }).then(result => {
+                if(result.isConfirmed) {
+                    this.contactCandidate(candidate);
+                }
+            });
+        },
+
+        contactCandidate(candidate) {
+            this.processingContactRequest = candidate;
+            axios.post('candidates/'+candidate.id+'/contact')
+                .then(res => {
+                    this.newContacts.push(candidate.id);
+                    this.coins = res.data.wallet_balance;
+                    this.processingContactRequest = null;
+
+                    this.$swal({
+                        toast: true,
+                        icon: 'success',
+                        position: 'top-end',
+                        showConfirmButton: false,
+                        timer: 3000,
+                        title: 'Contacted successfully'
+                    });
+                });
+        },
+
+        confirmHire(candidate) {
+            this.$swal({
+                icon: 'info',
+                html: 'Are you sure you want to hire this candidate, we will <strong>refund you 5 coins</strong> if you do, click confirm button to proceed',
+                showCancelButton: true,
+                confirmButtonText: 'Confirm'
+            }).then(result => {
+                if(result.isConfirmed) {
+                    this.hireCandidate(candidate);
+                }
+            });
+        },
+
+        hireCandidate(candidate) {
+            this.processingHireRequest = candidate;
+            axios.post('candidates/'+candidate.id+'/hire')
+                .then(res => {
+                    this.newHires.push(candidate.id);
+                    this.coins = res.data.wallet_balance;
+                    this.processingHireRequest = null;
+
+                    this.$swal({
+                        toast: true,
+                        icon: 'success',
+                        position: 'top-end',
+                        showConfirmButton: false,
+                        timer: 3000,
+                        title: 'Hired successfully'
+                    });
+                });
         }
     },
     mounted() {
